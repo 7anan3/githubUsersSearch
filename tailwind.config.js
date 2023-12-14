ea269f98b9c11bc8b1cdee5f4d6bb02277c84968
Hanane <hanane.elkaaba1@gmail.com>
2023-12-14 15:53:43 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+    colors: {
+      Navy: "rgb(25, 38, 85)",
+      blue1: "rgb(29, 38, 125)",
+      blue2: "rgb(0, 169, 255)",
+      white: "rgb(255, 255, 255)",
+    },
+  },
+  plugins: [],
+};
