247df7c8f741e7f8c34f82dd86e781a519398e25
Hanane <hanane.elkaaba1@gmail.com>
2023-12-19 16:52:29 +0100

    User Search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,23 +3,33 @@ import { AppContext } from "../GithubUsers";
 export default function SearchBar() {
   const { user, users } = useContext(AppContext);
   const [searchInput, setSearchInput] = useState("");
+  const [searchResult, setSearchResult] = useState(null);
   const handleSearch = (e) => {
     e.preventDefault();
-  };
 
+    const result = user.find((userr) => userr.login === searchInput);
+    if (result) {
+      setSearchResult(result);
+    } else {
+      setSearchResult(null);
+    }
+  };
+  console.log(searchResult);
   const handleSearchInput = (e) => {
     setSearchInput(e.target.value);
   };
 
   return (
-    <form onSubmit={handleSearch}>
+    <form>
       <input
         type="text"
         placeholder="Search Github username"
         value={searchInput}
         onChange={handleSearchInput}
       />
-      <button className="bg-blue2">Search</button>
+      <button className="bg-blue2" onClick={handleSearch}>
+        Search
+      </button>
     </form>
   );
 }
